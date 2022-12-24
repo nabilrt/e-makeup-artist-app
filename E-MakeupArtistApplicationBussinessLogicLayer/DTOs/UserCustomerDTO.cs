2fe0b7f4a90e91a07bf671b7a317a57208acf333
Ratul Bhattacharjee <77481058+Ratul41Bhatt@users.noreply.github.com>
2022-12-24 09:53:43 +0600

    UserCustomerDTO

--- E-MakeupArtistApplicationBussinessLogicLayer/DTOs/UserCustomerDTO.cs
+++ E-MakeupArtistApplicationBussinessLogicLayer/DTOs/UserCustomerDTO.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using E_MakeupArtistApplicationDataAccessLayer.DB.Models;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +10,32 @@ namespace E_MakeupArtistApplicationBussinessLogicLayer.DTOs
 {
     internal class UserCustomerDTO
     {
+        public int Id { get; set; }
+
+        public string Username { get; set; }
+
+        public string Password { get; set; }
+
+        public string Email { get; set; }
+
+        public string User_Type { get; set; }
+
+        public int Is_Approved { get; set; }
+
+
+        public string Name { get; set; }
+
+        public DateTime DOB { get; set; }
+
+       
+        public int AreaId { get; set; }
+
+        public string Address { get; set; }
+
+        public int Is_Premium { get; set; }
+
+     
+
+    
     }
 }
